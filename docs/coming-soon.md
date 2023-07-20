25e82d1b7fda4ac75f25aa49c4c3bf30fb18693a
Tnix <tnix@meower.org>
2023-07-20 23:44:20 +1200

    Initial commit

---
+++ docs/coming-soon.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Coming Soon
+
+Official Meower documentation coming soon!
---
+++ docusaurus.config.js
@@ -0,0 +1,83 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Meower Documentation',
+  tagline: 'Documentation for the Meower social media platform.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.meower.org',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'meower-media-co', // Usually your GitHub org/user name.
+  projectName: 'Meower-Docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/meower-media-co/Meower-Docs/tree/main/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      navbar: {
+        title: 'Meower Documentation',
+        logo: {
+          alt: 'Meowy Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'comingSoonSidebar',
+            position: 'left',
+            label: 'Coming Soon',
+          },
+          {
+            href: 'https://github.com/meower-media-co/Meower-Docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
